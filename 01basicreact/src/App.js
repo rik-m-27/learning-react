782171cd53d492f9a72d9ae0cf828ee41235c418
rik7662 <97977879+rik-m-27@users.noreply.github.com>
2024-06-15 19:38:22 +0530

    basic react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <>
+      <h2>classic react app</h2>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,7 @@
+function Youtube() {
+    return (
+        <h2>Youtube react app.</h2>
+    )
+}
+
+export default Youtube;
