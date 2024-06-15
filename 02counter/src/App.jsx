9c720626a655eb5b20b4e5a89261183b5c8c5209
rik7662 <97977879+rik-m-27@users.noreply.github.com>
2024-06-15 20:45:36 +0530

    counter property in react

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  // let counter = 0;
+  const [counter, setCounter] = useState(0); //initalizing counter with 0, setCounter is a method
+
+  const addValue = () =>{
+    // counter++;
+    // console.log(counter);
+    /**This is concept of batching. 
+     * react doesn't things immediately when you actually perform an action or state, 
+     * it actually bundles everything together.*/
+    // setCounter(counter+1);
+    // setCounter(counter+1);
+    // setCounter(counter+1);
+    /**if we want to call each and everyone, 
+     * than we have define our own call back method for setCounter */
+    setCounter((prevCounter) => {return (prevCounter+1);});
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+
+  const removeValue = () =>{
+    setCounter(counter-1);
+  }
+
+  return (
+    <>
+      <h1>React course with hitesh</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>Add value</button> {" "}
+      <button onClick={removeValue}>Remove value</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
