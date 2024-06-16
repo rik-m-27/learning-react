57c2b79b4a9200f627b400c3855af8dee8dcea2a
rik7662 <97977879+rik-m-27@users.noreply.github.com>
2024-06-17 00:09:35 +0530

    update App.jsx

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       <h2>Counter value : {counter}</h2>
       <button onClick={addValue}>Add value</button> {" "}
       <button onClick={removeValue}>Remove value</button>
-      <p>footer: {counter}</p>
+      <p>Learner: Rik</p>
     </>
   )
 }
