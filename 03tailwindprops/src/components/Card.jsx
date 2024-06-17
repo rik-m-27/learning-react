85cbdbe835eb651ea4571b569bbd85a236823356
rik7662 <97977879+rik-m-27@users.noreply.github.com>
2024-06-17 17:32:05 +0530

    final update

--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,23 +1,25 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 
-function Card() {
+function Card({username = "anonymous", post = "Not assigned yet"}) {
+    // console.log(props);
   return (
     <div>
-        <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-            <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://media.licdn.com/dms/image/D4D03AQE2tzfipn-yZQ/profile-displayphoto-shrink_800_800/0/1712704110862?e=1724284800&v=beta&t=evFNkVPFZEZAyUWP1V1OUnGK9lHMO9wHM0aSjlv_uDY" alt="" width="384" height="512"/>
-            <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+            <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://media.licdn.com/dms/image/D4D03AQE2tzfipn-yZQ/profile-displayphoto-shrink_800_800/0/1712704110862?e=1724284800&v=beta&t=evFNkVPFZEZAyUWP1V1OUnGK9lHMO9wHM0aSjlv_uDY" alt="" width="384" height="512"/>
+            <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
                 <blockquote>
-                <p class="text-lg font-medium">
+                <p className="text-lg font-medium">
                     “Tailwind CSS is the only framework that I've seen scale
-                    on large teams. It’s easy to customize, adapts to any design,
+                    on large teams. It's easy to customize, adapts to any design,
                     and the build size is tiny.”
                 </p>
                 </blockquote>
-                <figcaption class="font-medium">
-                <div class="text-sky-500 dark:text-sky-400">
-                    R I K
+                <figcaption className="font-medium">
+                <div className="text-sky-500 dark:text-sky-400">
+                    {username}
                 </div>
-                <div class="text-slate-700 dark:text-slate-500">
+                <div className="text-slate-700 dark:text-slate-500">
                     Software Engineer,Microsoft
                 </div>
                 </figcaption>
